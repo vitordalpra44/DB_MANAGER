1e8d6abea16dcb2f8d58c1401f50ded19211667e
Vitor Dalpra <vitordalpra@alunos.utfpr.edu.br>
2023-11-29 23:27:04 -0300

    ->

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -3,5 +3,5 @@ sys.path.append("../src/mysql")
 import mysql_connection
 import mysql_import_data
 
-connection = mysql_connection.mysql_establish_connection('localhost', 'root', 'athletico123', 'University')
+connection = mysql_connection.mysql_establish_connection('localhost', 'root', 'mudarasenha', 'University')
 mysql_import_data.saveTable('teaches', connection, 'University')
