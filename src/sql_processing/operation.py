fa57ae3192fa263a8df2c08e1e37dadc3e146fb0
D-S-F <danfer@alunos.utfpr.edu.br>
2023-11-30 20:44:20 -0300

    modificacoes where

--- src/sql_processing/operation.py
+++ src/sql_processing/operation.py
@@ -1,18 +1,28 @@
 import csv
 
-def load_tbl(filename):
-    with open(filename, 'r') as f:
-        reader = csv.reader(f, delimiter= ',')
-        return reader
+def load_tbl(table):
+    with open(table, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        return list(reader)
 
-def where_operation(where_val1, where_val2, reader):
-    with open('result.csv', 'w', newline= '') as result:
-        writer = csv.writer
-        for row in reader:
-            if (reader[where_val1] == where_val2):
-                writer.writerow(row)
+def where_operation(where_val1, where_val2, reader, operation, type):
+    result_rows = []
 
-        
+    for row in reader:
+        if operation == '=' and row[where_val1] == where_val2:
+            result_rows.append(row)
+        elif operation == '<' and row[where_val1] < where_val2:
+            result_rows.append(row)
+        elif operation == '>' and row[where_val1] > where_val2:
+            result_rows.append(row)
+        elif operation == '!' and row[where_val1] != where_val2:
+            result_rows.append(row)
+        elif operation == 'column' and row[where_val1] == row[where_val2]:
+            result_rows.append(row)
 
-def join_operation(tbl1, tbl2, value):
-    print('hello')
+    return result_rows
+
+def write_result(result_rows):
+    with open('result.csv', 'w', newline='') as result:
+        writer = csv.writer(result)
+        writer.writerows(result_rows)
