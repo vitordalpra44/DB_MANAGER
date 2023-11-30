3e10648b149acbc7edd6c61af2ed53d050363c75
D-S-F <danfer@alunos.utfpr.edu.br>
2023-11-30 20:11:03 -0300

    criacao ops

--- src/sql_processing/operation.py
+++ src/sql_processing/operation.py
@@ -1,2 +1,18 @@
-def where_operation(where_val1, where_val2, sig):
-    pass
+import csv
+
+def load_tbl(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter= ',')
+        return reader
+
+def where_operation(where_val1, where_val2, reader):
+    with open('result.csv', 'w', newline= '') as result:
+        writer = csv.writer
+        for row in reader:
+            if (reader[where_val1] == where_val2):
+                writer.writerow(row)
+
+        
+
+def join_operation(tbl1, tbl2, value):
+    print('hello')
