b4c6235e66c809ecb8cf8b06d1c283ca1b192b07
D-S-F <danfer@alunos.utfpr.edu.br>
2023-12-01 01:08:32 -0300

    funcs funcionando

--- src/sql_processing/operation.py
+++ src/sql_processing/operation.py
@@ -5,24 +5,98 @@ def load_tbl(table):
         reader = csv.reader(f, delimiter=',')
         return list(reader)
 
-def where_operation(where_val1, where_val2, reader, operation, type):
-    result_rows = []
+def cartesian (tbl1, tbl2):
+    tbl_result = []
 
-    for row in reader:
-        if operation == '=' and row[where_val1] == where_val2:
-            result_rows.append(row)
-        elif operation == '<' and row[where_val1] < where_val2:
-            result_rows.append(row)
-        elif operation == '>' and row[where_val1] > where_val2:
-            result_rows.append(row)
-        elif operation == '!' and row[where_val1] != where_val2:
-            result_rows.append(row)
-        elif operation == 'column' and row[where_val1] == row[where_val2]:
-            result_rows.append(row)
+    tbl_result.append(tbl1[0] + tbl2[0])
+    for a in range(1, len(tbl1)):
+        for b in range(1, len(tbl2)):
+            tbl_result.append(tbl1[a] + tbl2[b])
 
-    return result_rows
+    return tbl_result
 
-def write_result(result_rows):
-    with open('result.csv', 'w', newline='') as result:
-        writer = csv.writer(result)
-        writer.writerows(result_rows)
+def filter_column (tbl, col1, col2, op):
+    tbl_result = []
+    tbl_result.append(tbl[0])
+
+    if op == '=':
+        for row in range(1, len(tbl)):
+            if tbl[row][col1] == tbl[row][col2]:
+                tbl_result.append(tbl[row])
+    elif op == '!':
+        for row in range(1, len(tbl)):
+            if tbl[row][col1] != tbl[row][col2]:
+                tbl_result.append(tbl[row])
+    elif op == '<':
+        for row in range(1, len(tbl)):
+            if tbl[row][col1] < tbl[row][col2]:
+                tbl_result.append(tbl[row])
+    elif op == '>':
+        for row in range(1, len(tbl)):
+            if tbl[row][col1] > tbl[row][col2]:
+                tbl_result.append(tbl[row])
+
+    return tbl_result
+
+def filter_value (tbl, col, val, op):
+    tbl_result = []
+    tbl_result.append(tbl[0])
+
+    if op == '=':
+        for row in range(1, len(tbl)):
+            if tbl[row][col] == val:
+                tbl_result.append(tbl[row])
+    elif op == '!':
+        for row in range(1, len(tbl)):
+            if tbl[row][col] != val:
+                tbl_result.append(tbl[row])
+    elif op == '<':
+        for row in range(1, len(tbl)):
+            if tbl[row][col] < val:
+                tbl_result.append(tbl[row])
+    elif op == '>':
+        for row in range(1, len(tbl)):
+            if tbl[row][col] > val:
+                tbl_result.append(tbl[row])
+
+    return tbl_result
+
+def order (tbl, col, desc = False):
+    tbl.sort(key = lambda x : x[col], reverse= desc)
+
+def column_selection(tbl, cols):
+    tbl_result = []
+
+    for row in tbl:
+        n_row = []
+        col_count = 0
+        for c in row:
+            if col_count in cols:
+                n_row.append(c)
+            col_count += 1
+        tbl_result.append(n_row)
+    
+    return tbl_result
+
+#-----------------------------------------------------------#
+# Funcs extras caso necessário
+
+def return_column_n (tbl, val):
+    
+    count = 0
+    
+    for a in tbl[0]:
+        if tbl[0][a] == val:
+            return count
+    count += 1
+    
+    return (-1)
+
+def column_list (tbl, vals):
+    
+    list = []
+
+    for val in vals:
+        list.append(return_column_n(tbl, val))
+    
+    return list
